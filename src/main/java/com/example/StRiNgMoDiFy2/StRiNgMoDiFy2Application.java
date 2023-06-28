b4ad3ff3d59f2d008183085dda55810eb4719520
Name <email>
2023-06-28 19:58:42 +0200

    Initial commit

---
+++ src/main/java/com/example/StRiNgMoDiFy2/StRiNgMoDiFy2Application.java
@@ -0,0 +1,13 @@
+package com.example.StRiNgMoDiFy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StRiNgMoDiFy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StRiNgMoDiFy2Application.class, args);
+	}
+
+}
